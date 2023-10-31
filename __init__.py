7108e47579ed8e43fe29b7622c22e98619ab46fb
FleetCentere <mcgrathan@gmail.com>
2023-10-30 20:40:46 -0400

    new keys file

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import os
-from NHLProjectFiles.keys import secret_key
+from NHLProjectFiles.localkeys import secret_key
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
